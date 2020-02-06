5a7355c8f71645e570732cff2fffd2a76d09e470
cmcfadzean <court@kajabi.com>
2020-02-05 18:33:49 -0800

    adding migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_22_043955) do
+ActiveRecord::Schema.define(version: 2020_02_06_023315) do
+
+  create_table "offices", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "address"
+    t.string "city"
+    t.string "state"
+    t.integer "zip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
