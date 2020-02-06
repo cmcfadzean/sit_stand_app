c178cc9049872cee2031826d644047598dfbda51
cmcfadzean <court@kajabi.com>
2020-02-05 18:54:43 -0800

    adds users to offices

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_06_023315) do
+ActiveRecord::Schema.define(version: 2020_02_06_025248) do
 
   create_table "offices", force: :cascade do |t|
     t.string "name"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2020_02_06_023315) do
     t.integer "zip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_offices_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
